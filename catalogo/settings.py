6eb7ffc4b480ac9d12933e3a289a00ecbc3354a9
gustavopp93 <p1993.gustavo@gmail.com>
2016-05-04 17:28:00 -0500

    remove .idea. update allow hosts

--- catalogo/settings.py
+++ catalogo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '@^dgmj)ud$^m236dgz419&=zd=vuz12^gj%$mon6$)p)3%2nu5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
